c9caad21d36e97a08763623090c73d5ff4c125ea
eszterk90git config --global user.email eszter.kadar1@gmail.com <82053220+eszterk90@users.noreply.github.com>
2022-06-19 15:16:30 +0200

    first tutorial version

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
+import {Suspense, useState} from 'react'
+import {Canvas} from '@react-three/fiber'
+import {OrbitControls} from '@react-three/drei'
+import Archer from './components/Archer'
 
 function App() {
+
+  const [action, setAction] = useState('StandingIdle');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <Canvas>
+      <ambientLight />
+        <pointLight intensity={2} position={[-1, 1, 3]} color="red" />
+        <pointLight intensity={2} position={[1, 1, 3]} color="blue" />
+        <pointLight intensity={2} position={[0, 3, -10]} color="white" />
+      <Suspense fallback={null}>
+        <Archer action={action}/>
+      </Suspense>
+      <OrbitControls />
+    </Canvas>
+    <div className='controls'>
+      <button onClick={() => {setAction('RunForward')}}>Run Forward</button>
+      <button onClick={() => {setAction('Death')}}>Death</button>
+      <button onClick={() => {setAction('DrawArrow')}}>Draw Arrow</button>
+      <button onClick={() => {setAction('StandingIdle')}}>Idle</button>
     </div>
+    </>
   );
 }
 
